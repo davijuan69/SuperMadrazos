8c52abbecc9be52c8c5605edd7bf23a96cbd2808
juan david <jdacevedo.24@est.ucab.edu.ve>
2025-08-16 17:38:25 -0400

    feat: añadi salto

--- My project/Assets/script/movimiento.cs
+++ My project/Assets/script/movimiento.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class movimiento : MonoBehaviour{
     public float velocidad = 5f;
-    public float salto = 5f;
+    public float salto = 6f;
     public bool enSuelo = false;
     private Rigidbody2D rb;
     private float movimientoHorizontal;
+    public float distanciaRaycast = 1.2f;
+    public LayerMask capaSuelo;
 
     void Start()
     {
@@ -17,7 +19,15 @@ public class movimiento : MonoBehaviour{
         movimientoHorizontal = Input.GetAxis("Horizontal");
         //float movimientoVertical = Input.GetAxis("Vertical");
 
-        if(Input.GetKeyDown(KeyCode.W)){
+        DetectarSuelo();
+
+        //if(enSuelo) {
+        //Debug.Log("Está en el suelo");
+        //} else {
+        //Debug.Log("NO está en el suelo");
+        //}
+
+        if(Input.GetKeyDown(KeyCode.W) && enSuelo){
             saltar();
         }
     }
@@ -25,6 +35,18 @@ public class movimiento : MonoBehaviour{
     void FixedUpdate(){
         Vector2 nuevaVelocidad = new Vector2(movimientoHorizontal * velocidad, rb.linearVelocity.y);
         rb.linearVelocity = nuevaVelocidad;
+    }
+
+    void DetectarSuelo(){
+    // Posición del personaje
+    Vector2 posicion = transform.position;
+     posicion.y -= 0.5f;
+    
+    // Dispara raycast hacia abajo
+    RaycastHit2D hit = Physics2D.Raycast(posicion, Vector2.down, distanciaRaycast, capaSuelo);
+    
+    // Si toca algo, está en el suelo
+    enSuelo = hit.collider != null;
     }
 
     void saltar(){
